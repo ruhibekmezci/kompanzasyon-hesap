4658858c7ff3557591dac5b32ea1e07b32e0d872
Ruhi Atakan Bekmezci <58533173+ruhibekmezci@users.noreply.github.com>
2025-11-25 01:09:30 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('komp-store').then((cache) => cache.addAll([
+      'index.html',
+      'manifest.json',
+      'icon.png'
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
